83c1e22298f2d3f0f1f28bb3b24111598f4ecc70
nicholasjpaterno <cashlion@protonmail.com>
2020-04-20 22:26:55 -0400

    Refactor test to typescript

--- src/test.ts
+++ src/test.ts
@@ -1,15 +1,16 @@
-const MaFP = require("./index");
-const test = new MaFP();
-test.set("A", true);
-test.set("B", false);
-test.set("C", true);
-test.set("D", true);
+import MaFP from "./index";
+const test = new MaFP<string, boolean>([
+  ["A", true],
+  ["B", false],
+  ["C", true],
+  ["D", true],
+]);
 
 const resFilterMap = test.filter(val => val);
 const resFilterArr = test.filterToArray(val => val);
 const resMapMap = test.map(val => !val);
 const resMapArr = test.mapToArray(val => !val);
-const reduce = test.reduce((acc, curr) => acc + curr, 0);
+const reduce = test.reduce((acc, curr) => acc + Number(curr), 0);
 
 console.log(resFilterMap);
 console.log(resFilterArr);
