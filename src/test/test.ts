9a7aaec718e3db77a41209af91eb5f7a0f8ec73b
nicholasjpaterno <cashlion@protonmail.com>
2020-04-23 00:16:20 -0400

    Test MaFP vs Object

--- src/test/test.ts
+++ src/test/test.ts
@@ -17,3 +17,32 @@ console.log(resFilterArr);
 console.log(resMapMap);
 console.log(resMapArr);
 console.log(reduce);
+
+const loops = [ 10, 100, 1000, 10000, 100000, 1000000 ];
+
+const sleep = (time = 1000) => new Promise(resolve => setTimeout(resolve, time));
+
+const run = async () => {
+  for (const loop of loops) {
+    const obj: {[key: string]: boolean} = {};
+    const mafp = new MaFP<number, boolean>();
+    sleep(100);
+    for (let i = 0; i < loop; i++) {
+      const val = !!i;
+      mafp.set(i, val);
+      obj[i] = val;
+    }
+    sleep(1000);
+    console.time(`${loop} obj entries`);
+    Object.entries(obj).filter(entry => entry[1]);
+    // Object.keys(obj).filter(val => obj[val]);
+    console.timeEnd(`${loop} obj entries`);
+    sleep(1000);
+    console.time(`${loop} mafp entries`);
+    mafp.filter(val => val);
+    console.timeEnd(`${loop} mafp entries`);
+    sleep(1000);
+  }
+};
+
+run();
