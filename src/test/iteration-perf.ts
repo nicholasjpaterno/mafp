bc867d80401b043ef57853e78cf763794ef4502c
nicholasjpaterno <cashlion@protonmail.com>
2020-04-25 19:40:04 -0400

    Update tests to linter recommendations

--- src/test/iteration-perf.ts
+++ src/test/iteration-perf.ts
@@ -1,38 +1,36 @@
-console.time("APP");
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-empty */
+console.time('APP');
 const testMap = new Map();
 
-console.log("Setting up");
+console.log('Setting up');
 
 for (let i = 0; i < 1000000; i++) {
   testMap.set(i, i.toString());
 }
-console.log("Testing..\n");
+console.log('Testing..\n');
 
-console.time("forEach");
-testMap.forEach(function (_value, _key) {
-    
-});
-console.timeEnd("forEach");
+console.time('forEach');
+// eslint-disable-next-line prettier/prettier
+testMap.forEach(function (_value, _key) { });
+console.timeEnd('forEach');
 
-console.time("entry");
+console.time('entry');
 for (const _entry of testMap) {
-  
 }
-console.timeEnd("entry");
+console.timeEnd('entry');
 
-
-console.time("destructure");
+console.time('destructure');
 for (const [_key, _value] of testMap) {
-  
 }
-console.timeEnd("destructure");
+console.timeEnd('destructure');
 
-
-console.time("local");
+console.time('local');
 for (const entry of testMap) {
   const _key = entry[0];
   const _value = entry[1];
 }
-console.timeEnd("local");
+console.timeEnd('local');
 
-console.timeEnd("APP");
+console.timeEnd('APP');
--- src/test/test.ts
+++ src/test/test.ts
@@ -1,18 +1,18 @@
-import MaFP from "../index";
+import MaFP from '../index';
 const test = new MaFP([
-  ["A", true],
-  ["B", false],
-  ["C", true],
-  ["D", true],
+  ['A', true],
+  ['B', false],
+  ['C', true],
+  ['D', true],
 ]);
 
-const resFilterMap = test.filter(val => val);
-const resFilterArr = test.filterToArray(val => val);
-const resMapMap = test.map(val => !val);
-const resMapArr = test.mapToArray(val => !val);
+const resFilterMap = test.filter((val) => val);
+const resFilterArr = test.filterToArray((val) => val);
+const resMapMap = test.map((val) => !val);
+const resMapArr = test.mapToArray((val) => !val);
 const reduce = test.reduce((acc, curr) => acc + Number(curr), 0);
-const every = test.every(val => val);
-const some = test.some(val => val);
+const every = test.every((val) => val);
+const some = test.some((val) => val);
 
 console.log(resFilterMap);
 console.log(resFilterArr);
@@ -22,13 +22,14 @@ console.log(reduce);
 console.log(every);
 console.log(some);
 
-const loops = [ 10, 100, 1000, 10000, 100000, 1000000 ];
+const loops = [10, 100, 1000, 10000, 100000, 1000000];
 
-const sleep = (time = 1000) => new Promise(resolve => setTimeout(resolve, time));
+const sleep = (time = 1000): Promise<unknown> =>
+  new Promise((resolve) => setTimeout(resolve, time));
 
-const run = async () => {
+const run = (): void => {
   for (const loop of loops) {
-    const obj: {[key: string]: boolean} = {};
+    const obj: { [key: string]: boolean } = {};
     const mafp = new MaFP<number, boolean>();
     sleep(100);
     for (let i = 0; i < loop; i++) {
@@ -38,12 +39,12 @@ const run = async () => {
     }
     sleep(1000);
     console.time(`${loop} obj entries`);
-    Object.entries(obj).filter(entry => entry[1]);
+    Object.entries(obj).filter((entry) => entry[1]);
     // Object.keys(obj).filter(val => obj[val]);
     console.timeEnd(`${loop} obj entries`);
     sleep(1000);
     console.time(`${loop} mafp entries`);
-    mafp.filter(val => val);
+    mafp.filter((val) => val);
     console.timeEnd(`${loop} mafp entries`);
     sleep(1000);
   }
@@ -53,7 +54,7 @@ run();
 
 const testKeys = test.keys();
 console.log(testKeys);
-const resKeys = testKeys.filterToArray(val => !!val);
+const resKeys = testKeys.filterToArray((val) => !!val);
 console.log(resKeys);
 
 for (const iterator of testKeys) {
@@ -62,7 +63,7 @@ for (const iterator of testKeys) {
 
 const testVals = test.values();
 console.log(testVals);
-const res = testVals.filterToArray(val => !!val);
+const res = testVals.filterToArray((val) => !!val);
 console.log(res);
 
 for (const iterator of testVals) {
